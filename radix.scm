0ce45a3e773bce3debbcde70faf8910b80333bf0
valvallow <valvalloooooooooow@gmail.com>
2013-07-06 01:50:52 +0900

    vector version added

--- radix.scm
+++ radix.scm
@@ -23,9 +23,9 @@
             (rec (apply append (map reverse buckets))(+ index 1)))
           ls))))
 
-(radix-sort (shuffle (iota 10)))
 
 (use gauche.sequence)
+
 (define (test sorter n)
   (for-each (^i (let1 ls (shuffle (iota (expt 10 i)))
                   (print "; length = " (expt 10 i))
@@ -70,3 +70,70 @@
 ; real 242.686
 ; user 241.360
 ; sys    0.610
+
+
+
+;; vector version
+(use srfi-43)
+(define (radix-sort ls :optional (base 10))
+  (define (digit-count num)
+    (x->integer (ceiling (/ (log num)(log base)))))
+  (define (digit-of index num)
+    (modulo (quotient num (expt base index)) base))
+  (define (put-buckets! vect buckets index)
+    (vector-for-each
+     (^(_ e)
+       (let1 digit (digit-of index e)
+         (vector-set! buckets digit
+                      (cons e (vector-ref buckets digit)))))
+     vect))
+  (let ((buckets (make-vector base '()))
+        (v (list->vector ls)))
+    (dotimes (index (digit-count (abs (apply max ls))))
+      (put-buckets! v buckets index)
+      (set! v (list->vector (apply append (map reverse (vector->list buckets)))))
+      (set! buckets (make-vector base '())))
+    (vector->list v)))
+
+(test radix-sort 6)
+
+; length = 100
+;(time (sorter ls))
+; real   0.000
+; user   0.010
+; sys    0.000
+
+; length = 1000
+;(time (sorter ls))
+; real   0.001
+; user   0.000
+; sys    0.000
+
+; length = 10000
+;(time (sorter ls))
+; real   0.015
+; user   0.020
+; sys    0.000
+
+; length = 100000
+;(time (sorter ls))
+; real   0.203
+; user   0.210
+; sys    0.000
+
+; length = 1000000
+;(time (sorter ls))
+; real   2.309
+; user   2.300
+; sys    0.000
+
+; length = 10000000
+GC Warning: Repeated allocation of very large block (appr. size 80003072):
+	May lead to memory leak and poor performance.
+GC Warning: Repeated allocation of very large block (appr. size 80003072):
+	May lead to memory leak and poor performance.
+;(time (sorter ls))
+; real  65.467
+; user  35.900
+; sys    3.370
+
