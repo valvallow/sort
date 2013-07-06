c064f8bf6b74bdc1eea71f7628a73f7e6ce6acb5
valvallow <valvalloooooooooow@gmail.com>
2013-07-06 11:24:06 +0900

    refactoring

--- merge.scm
+++ merge.scm
@@ -2,9 +2,12 @@
 (define (merge-sort ls)
   (define (merge ls1 ls2)
     (let rec ((ls1 ls1)(ls2 ls2)(acc '()))
-      (cond ((null? ls1)(reverse (append (reverse ls2) acc)))
-            ((null? ls2)(reverse (append (reverse ls1) acc)))
-            ((< (car ls1)(car ls2))(rec (cdr ls1) ls2 (cons (car ls1) acc)))
+      (cond ((null? ls1)
+             (reverse (append (reverse ls2) acc)))
+            ((null? ls2)
+             (reverse (append (reverse ls1) acc)))
+            ((< (car ls1)(car ls2))
+             (rec (cdr ls1) ls2 (cons (car ls1) acc)))
             (else (rec ls1 (cdr ls2)(cons (car ls2) acc))))))
   (if (or (null? ls)
           (null? (cdr ls)))
@@ -14,6 +17,8 @@
             (split-at ls split-index)
           (merge (merge-sort head)
                  (merge-sort tail))))))
+
+
 
 (use gauche.sequence)
 (define (test sorter n)
@@ -35,30 +40,29 @@
 ; length = 1000
 ;(time (sorter ls))
 ; real   0.002
-; user   0.000
+; user   0.010
 ; sys    0.000
 
 ; length = 10000
 ;(time (sorter ls))
-; real   0.027
+; real   0.025
 ; user   0.030
 ; sys    0.000
 
 ; length = 100000
 ;(time (sorter ls))
-; real   0.283
+; real   0.288
 ; user   0.280
-; sys    0.010
+; sys    0.000
 
 ; length = 1000000
 ;(time (sorter ls))
-; real   3.300
-; user   3.250
+; real   3.422
+; user   3.370
 ; sys    0.030
 
 ; length = 10000000
 ;(time (sorter ls))
-; real  47.933
-; user  47.480
-; sys    0.310
-
+; real  40.993
+; user  40.620
+; sys    0.240
